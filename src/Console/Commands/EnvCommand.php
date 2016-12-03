f9189ac76913b1cb2e6dfc3a4cf08296f9c77ee8
Brian Faust <hello@brianfaust.de>
2016-12-03 06:38:50 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Console/Commands/EnvCommand.php
+++ src/Console/Commands/EnvCommand.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Laravel Environment.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Environment\Console\Commands;
 
 use Illuminate\Console\Command;
@@ -23,7 +32,7 @@ abstract class EnvCommand extends Command
 
         $this->files = [
             'encrypted' => base_path('.env'),
-            'backup' => base_path('.env.backup'),
+            'backup'    => base_path('.env.backup'),
             'decrypted' => base_path('.env.decrypted'),
         ];
     }
