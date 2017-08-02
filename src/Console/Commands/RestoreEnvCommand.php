7defa36945212adc17ec3aade2a55c1f89be44ea
Brian Faust <faustbrian@users.noreply.github.com>
2017-08-02 12:06:39 +0300

    Apply fixes from StyleCI (#15)
    [ci skip] [skip ci]

--- src/Console/Commands/RestoreEnvCommand.php
+++ src/Console/Commands/RestoreEnvCommand.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Laravel Environment.
  *
- * (c) Brian Faust <hello@brianfaust.me>
+ * (c) Brian Faust <hello@brianfaust.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
